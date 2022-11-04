995ecd2e5ea311bcc6e150d9170d8cf21b7979d4
mohamedadel80080 <mohamedadel80080@gmail.com>
2022-11-04 17:14:32 +0200

    result

---
+++ database/migrations/2022_11_04_135801_create_profile_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('phone');
+            $table->string('description');
+            $table->string('study'); //foreign
+            $table->string('work'); //foreign
+            $table->string('facebook');
+            $table->string('linkedin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+};
